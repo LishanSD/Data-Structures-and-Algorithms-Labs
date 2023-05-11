af517c16d1692fd224e0604576194ef2d91baf32
LishanSD <129363237+LishanSD@users.noreply.github.com>
2023-05-11 17:37:02 +0530

    Answers for the lab 6

---
+++ Lab_6_210339J/In_class_6_2.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace std::chrono;
+
+struct Node {
+   int data;
+   struct Node *next;
+};
+struct Node* top = NULL;
+void Push(int val) {
+   struct Node* newnode = (struct Node*) malloc(sizeof(struct Node));
+   newnode->data = val;
+   newnode->next = top;
+   top = newnode;
+}
+void Pop() {
+   if(top==NULL)
+   cout<<"Stack Underflow"<<endl;
+   else {
+      top = top->next;
+   }
+}
+void Display() {
+   struct Node* ptr;
+   if(top==NULL)
+   cout<<"stack is empty";
+   else {
+      ptr = top;
+      cout<<"Stack elements are: ";
+      while (ptr != NULL) {
+         cout<< ptr->data <<" ";
+         ptr = ptr->next;
+      }
+   }
+   cout<<endl;
+}
+int main() {
+    vector<int> values(10000);
+    auto f = []() -> int { return rand() % 10000; };
+    generate(values.begin(), values.end(), f);
+    auto start = high_resolution_clock::now();
+
+    Push(8);
+    Push(10);
+    Push(5);
+    Push(11);
+    Push(15);
+    Push(23);
+    Push(6);
+    Push(18);
+    Push(20);
+    Push(17);
+    Display();
+    Pop();
+    Pop();
+    Pop();
+    Pop();
+    Pop();  
+    Display();
+    Push(4);
+    Push(30);
+    Push(3);
+    Push(1);
+    Display();
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: "
+         << duration.count() << " microseconds" << endl;
+
+   return 0;
+}
